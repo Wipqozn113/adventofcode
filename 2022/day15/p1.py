6149a3b10e83f4f8e6a67dd2e17247cafd46160b
Bryan Cave <bryan.d.cave@gmail.com>
2022-12-15 08:23:54 -0400

    Start day 15; created coordinates uitl

---
+++ 2022/day15/p1.py
@@ -0,0 +1,39 @@
+# Inject into python path
+import sys
+import os
+import re 
+m = re.search(r"\d+", s)
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+
+from utils.coordinates import Coordinate
+
+class Beacon:
+    def __init__(self, x, y):
+        self.coord = Coordinate(x, y)
+
+    def __eq__(self, other):
+        return self.coord == other.coord
+
+class Sensor:
+    def __init__(self, x, y, beacon):
+        self.coord = Coordinate(x, y)
+        self.closest_beacon = beacon
+
+def CreateSensors(filename):
+    sensors = []
+    beacons = set()
+    with open(filename) as file:
+        for line in file:
+            line = line.strip()
+            matches = re.findall(r"-?\d+", line)
+            beacon = Beacon(matches[2], matches[3])      
+            # Duplicate beacon     
+            for bc in beacons:
+                if beacon == bc:
+                    beacon = bc
+            sensor = Sensor(matches[0], matches[1], beacon)
+            sensors.append(sensor)
+            beacons.add(beacon)
+
+            
