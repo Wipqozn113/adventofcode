f909b7c4a417f4d92dd369098fcc67335fe071e5
Bryan Cave <bryan.d.cave@gmail.com>
2022-12-21 11:21:24 -0400

    Update p2.py

--- 2022/day21/p2.py
+++ 2022/day21/p2.py
@@ -1,6 +1,9 @@
+from sympy import symbols, solve
+from sympy.parsing.sympy_parser import parse_expr
+
 class Monkey:
     def __init__(self, name, job):
-        self.name = name
+        self.name = name.strip()
         self.job = job
         self.val = None
         if isinstance(job, int):
@@ -12,12 +15,28 @@ class Monkey:
             self.mon2name = job[2].strip()
             self.op = job[1].strip()
             if name == "root":
-                self.op = "=="
+                self.op = "="
 
     def FinishJob(self, monkeys):
         if self.val is None:
             self.mon1 = monkeys[self.mon1name]
             self.mon2 = monkeys[self.mon2name]
+
+    def CreateFullJob(self):
+        return self.JobsToString()
+
+    def JobsToString(self):
+        if self.name == "humn":
+            return "x"
+
+        if self.val is not None:
+            return str(self.val)        
+
+        mon1 = self.mon1.JobsToString()
+        mon2 = self.mon2.JobsToString()
+        job = "({} {} {})".format(mon1, self.op, mon2)
+
+        return job
 
     def RootJob(self):
         return self.mon1.DoJob() == self.mon2.DoJob()
@@ -54,16 +73,30 @@ def CreateMonkeys(filename):
 
     return monkeys
 
-def FindHumnNumber(monkeys):
+# 142000
+def BruteForceHumnNumber(monkeys):
     humn = monkeys["humn"]
     root = monkeys["root"]
     num = 0
     while True:
+        if num % 1000 == 0:
+            print("Testing number ", num)
         humn.val = num
         if root.RootJob():
             print(num)
             return num
         num += 1
 
+# Because fucking syntax error annoyance
+# https://www.mathpapa.com/algebra-calculator.html
+def DiscoverHumnNumber(monkeys):
+    root = monkeys["root"]
+    equation = root.CreateFullJob()
+    equations = equation.split("=")
+    eqX = equations[0] if 'x' in equations[0] else equations[1]
+    expr = parse_expr(eqX)
+    sol = solve(expr)
+    print(sol[0])
+
 monkeys = CreateMonkeys("input.in")
-FindHumnNumber(monkeys)
+DiscoverHumnNumber(monkeys)
