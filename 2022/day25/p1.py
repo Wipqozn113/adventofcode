7eccbb274d8faa2526cc27e958a3e3da88a594f2
Bryan Cave <bryan.d.cave@gmail.com>
2023-03-30 14:15:46 -0300

    started day 25 (deja vu)

---
+++ 2022/day25/p1.py
@@ -0,0 +1,80 @@
+class SNAFU:
+    def __init__(self):
+        self.snafu = None
+        self.dec = None
+
+    def SetSnafu(self, snafu):
+        self.snafu = snafu
+    
+    def SetDecimal(self, decimal):
+        self.dec = decimal
+
+    @property
+    def AsSnafu(self):
+        if self.snafu is not None:
+            return self.snafu
+        elif self.dec is not None:
+            return self._ConvertToSnafu()
+        else:
+            return None
+
+    @property
+    def AsDecimal(self):
+        if self.dec is not None:
+            return self.dec
+        elif self.snafu is not None:
+            return self._ConvertToDecimal()
+        else:
+            return None
+        
+    def _ConvertToSnafu(self):
+        if self.snafu is not None:
+            return self.snafu
+
+        if self.dec is None:
+            return ""
+
+        return self.snafu  
+        
+    def _ConvertToDecimal(self):
+        if self.dec is not None:
+            return self.dec
+
+        if self.snafu is None:
+            return 0            
+
+        i = 0
+        ln = len(self.snafu)
+        num = 0
+        while i < ln:
+            x = ln - i - 1
+            n = 0
+            if self.snafu[i] == "-":
+                n = -1
+            elif self.snafu[i] == "=":
+                n = -2
+            else:
+                n = int(self.snafu[i])
+            
+            num += (5**x) * n
+            i += 1
+
+        self.dec = num
+        return self.dec       
+
+def CalculateSum(filename = "test.in"):
+    with open(filename) as file:
+        total = 0
+        for line in file:
+            snafu = SNAFU()
+            snafu.SetSnafu(line.strip())
+            total += snafu.AsDecimal
+
+    snafu = SNAFU()
+    snafu.SetDecimal(total)
+
+    return snafu
+
+total = CalculateSum()
+print("Decimal: ", total.AsDecimal)
+print("SNAFU: ", total.AsSnafu)
