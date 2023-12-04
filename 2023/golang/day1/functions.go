980c12d8fc00a31eef91b78d12412de70238a5f7
Bryan Cave <bryan.d.cave@gmail.com>
2023-12-04 16:52:31 -0400

    part 2

--- 2023/golang/day1/functions.go
+++ 2023/golang/day1/functions.go
@@ -41,3 +41,56 @@ func FindLast(line string) (lastnum int) {
 
 	return
 }
+
+func FindFirstWithText(line string) (firstnum int) {
+	nums := [18]string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+	firstpos := 999999999999999999
+
+	for _, num := range nums {
+		if first := strings.Index(line, num); first < firstpos && first > -1 {
+			firstpos = first
+			firstnum = MapTextToNumbers(num)
+		}
+	}
+
+	return
+}
+
+func FindLastWithText(line string) (lastnum int) {
+	nums := [18]string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+	lastpos := -1
+
+	for _, num := range nums {
+		if last := strings.LastIndex(line, num); last > lastpos && last > -1 {
+			lastpos = last
+			lastnum = MapTextToNumbers(num)
+		}
+	}
+
+	return
+}
+
+func MapTextToNumbers(text string) int {
+	mappings := map[string]int{
+		"one":   1,
+		"two":   2,
+		"three": 3,
+		"four":  4,
+		"five":  5,
+		"six":   6,
+		"seven": 7,
+		"eight": 8,
+		"nine":  9,
+		"1":     1,
+		"2":     2,
+		"3":     3,
+		"4":     4,
+		"5":     5,
+		"6":     6,
+		"7":     7,
+		"8":     8,
+		"9":     9,
+	}
+
+	return mappings[text]
+}
--- 2023/golang/day1/main.go
+++ 2023/golang/day1/main.go
@@ -15,16 +15,21 @@ func main() {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	total := 0
+	total1 := 0
+	total2 := 0
 	// optionally, resize scanner's capacity for lines over 64K, see next example
 	for scanner.Scan() {
 		line := scanner.Text()
 		first := FindFirst(line)
 		last := FindLast(line)
-		total += (first * 10) + last
+		total1 += (first * 10) + last
+		first = FindFirstWithText(line)
+		last = FindLastWithText(line)
+		total2 += (first * 10) + last
 	}
 
-	fmt.Println(total)
+	fmt.Println(total1)
+	fmt.Println(total2)
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
