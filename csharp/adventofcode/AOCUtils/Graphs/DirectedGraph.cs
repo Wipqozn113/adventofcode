b23d5fab1ab8a5f30fcb9e3f2b8e069b0edbb96e
Bryan Cave <bryan.d.cave@gmail.com>
2023-12-08 21:36:29 -0400

    Create DirectedGraph.cs

---
+++ csharp/adventofcode/AOCUtils/Graphs/DirectedGraph.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOCUtils.Graphs
+{
+    public class DirectedGraph
+    {
+        private class Node
+        {
+            public Node(string name)
+            {
+                Name = name;
+            }
+
+            public string Name { get; set; }
+
+            public List<Edge> Edges { get; set; } = new List<Edge>();
+
+            public Dictionary<string, long> DistanceTo { get; set; } = new Dictionary<string, long>();
+
+        }
+
+        private class Edge
+        {
+            public Edge(Node parent, Node child, long weight)
+            {
+                Parent = parent;
+                Child = child;
+                Weight = weight;
+            }
+
+            public Node Parent { get; set; }
+
+            public Node Child { get; set; }
+
+            public long Weight { get; set; }
+        }
+    }
+}
