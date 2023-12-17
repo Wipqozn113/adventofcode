fe470d95338e05bc41cb0b87e48446a004ce10e6
Bryan Cave <bryan.d.cave@gmail.com>
2023-12-17 11:50:37 -0400

    basic dijkstra

--- csharp/adventofcode/2023/Day17/Graph.cs
+++ csharp/adventofcode/2023/Day17/Graph.cs
@@ -15,10 +15,83 @@ namespace AOC2023.Day17
 
         public List<List<Node>> Nodes { get; set; } = new List<List<Node>>();
 
-        public long FindLowestHeatLoss()
+        public Node LavaPool
         {
-            return 0;
+            get
+            {
+                return Nodes[0][0];
+            }
         }
+
+        public Node Factory
+        {
+            get
+            {
+                return Nodes.Last().Last();
+            }
+        }
+
+        public double FindLowestHeatLoss()
+        {
+            return Dijkstra(LavaPool, Factory);
+        }
+
+        private double Dijkstra(Node source, Node target)
+        {
+            // Initialize all distances (except source to source) to Infinity
+            var nodes = Nodes.SelectMany(n => n).ToList();
+            var queue = new List<Node>();
+            var distances = new Dictionary<Node, double>();
+            distances[source] = 0;
+            foreach(var node in nodes)
+            {
+                if(node != source)
+                {
+                    distances[node] = double.PositiveInfinity;
+                }
+                queue.Add(node);
+            }
+
+            // Calculate shortest distances
+            while(queue.Any())
+            {
+                var keyValuePair = distances.Where(n => queue.Contains(n.Key)).OrderBy(x => x.Value).First();
+                var node = keyValuePair.Key;
+                queue.Remove(node);
+                
+                foreach(var edge in node.Edges)
+                {
+                    var alt = distances[node] + edge.HeatLoss;
+                    if(alt < distances[edge.Target])
+                    {
+                        distances[edge.Target] = alt;
+                    }
+                }
+            }
+
+            return distances[target];
+
+        }
+
+        /*
+         * function Dijkstra(Graph, source):
+       dist[source]  := 0                     // Distance from source to source is set to 0
+       for each vertex v in Graph:            // Initializations
+           if v ≠ source
+               dist[v]  := infinity           // Unknown distance function from source to each node set to infinity
+           add v to Q                         // All nodes initially in Q
+
+      while Q is not empty:                  // The main loop
+          v := vertex in Q with min dist[v]  // In the first run-through, this vertex is the source node
+          remove v from Q 
+
+          for each neighbor u of v:           // where neighbor u has not yet been removed from Q.
+              alt := dist[v] + length(v, u)
+              if alt < dist[u]:               // A shorter path to u has been found
+                  dist[u]  := alt            // Update distance of u 
+
+      return dist[]
+  end function */
 
         private void ParseInput(List<string> lines)
         {
