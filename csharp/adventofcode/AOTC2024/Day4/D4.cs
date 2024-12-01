22c04a782edf0918633a8461cbabf24944e2b35b
Bryan Cave <bryan.d.cave@gmail.com>
2024-12-01 18:02:07 -0400

    Putting in skeletons for next week

---
+++ csharp/adventofcode/AOTC2024/Day4/D4.cs
@@ -0,0 +1,28 @@
+﻿using AOTC2024.Day1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOTC2024.Day4
+{
+    public class D4
+    {
+        public static void Part1()
+        {
+            Console.WriteLine("Part 1: NOT IMPLEMENTED");
+        }
+
+        public static void Part2()
+        {
+            Console.WriteLine("Part 2: NOT IMPLEMENTED");
+        }
+
+        public static void ParseInputFromFile(string filename)
+        {
+            string path = $"{Directory.GetParent(Environment.CurrentDirectory)?.Parent?.Parent?.FullName}\\Day4\\{filename}";
+            var lines = File.ReadLines(path);
+        }
+    }
+}
--- csharp/adventofcode/AOTC2024/Program.cs
+++ csharp/adventofcode/AOTC2024/Program.cs
@@ -1,4 +1,12 @@
 ﻿using AOTC2024.Day1;
+using AOTC2024.Day2;
+using AOTC2024.Day3;
+using AOTC2024.Day4;
+using AOTC2024.Day5;
+using AOTC2024.Day6;
+using AOTC2024.Day7;
+using AOTC2024.Day8;
+
 
 Console.WriteLine("Which day would you like to run? Enter EXIT to end program.\n");
 string? day = Console.ReadLine();
@@ -10,6 +18,52 @@ while (day is not null && day.ToLower() != "exit")
         D1.Part2();
     }
 
+    else if (day == "2")
+    {
+        D2.Part1();
+        D2.Part2();
+    }
+
+    else if (day == "3")
+    {
+        D3.Part1();
+        D3.Part2();
+    }
+
+    else if (day == "4")
+    {
+        D4.Part1();
+        D4.Part2();
+    }
+
+    else if (day == "5")
+    {
+        D5.Part1();
+        D5.Part2();
+    }
+
+    else if (day == "6")
+    {
+        D6.Part1();
+        D6.Part2();
+    }
+
+    else if (day == "7")
+    {
+        D7.Part1();
+        D7.Part2();
+    }
+
+    else if (day == "8")
+    {
+        D8.Part1();
+        D8.Part2();
+    }
+
+    else
+    {
+        Console.WriteLine("Day not recoginzed.");
+    }
 
     Console.WriteLine("Which day would you like to run?  Enter EXIT to end program.\n");
     day = Console.ReadLine();
